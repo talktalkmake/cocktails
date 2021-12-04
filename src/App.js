0a6257c4c1eef60c9eef43a1338cdf24c60fc7fa
Tom Hart <kitsonian@gmail.com>
2021-12-04 16:45:46 -0500

    making progress with components

--- src/App.js
+++ src/App.js
@@ -3,54 +3,76 @@ import './App.css'
 
 function App() {
   const [data, setData] =                       useState(false)
-  const [type, setType] =                       useState('random')
+  const [type, setType] =                       useState("random")
   const [viewSingleDrink, setViewSingleDrink] = useState(false)
-  const [searchTerm, setSearchTerm] =           useState('gin')
-  // useEffect(() => {
-  //   console.log(type, data);
-  //   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-  //     .then(res => res.json())
-  //     .then(json => setData(json))
-  // .catch(error => console.log(error))
-  // }, [type])
-  useEffect(() => {
+  const [searchTerm, setSearchTerm] =           useState(false)
 
-    (async () => {
-      try {
-        const response =  await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + searchTerm)
-        const json =      await response.json()
-        setData(json)
-      } catch(error) {
-        console.log(error)
-      }
-    })()
+  async function fetchRandomDrinks(){
+    try {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const json = await response.json();
+      setData(json);
+      console.log(json)
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
 
-  }, [searchTerm])
-  return (
-    <>
-      <h1>Drinks!</h1>
-      <button onClick={() => setType(Math.random())}>Get a random cocktail</button>
+  const getRandomDrinks = () => {
+    return (
+      <>
+      <h1>Try a Random Drink</h1>
+      <button onClick={() => fetchRandomDrinks()}>Get a random cocktail</button>
       <label htmlFor="search">Search by ingredient</label>
-      <input type="text" onChange={event => {setSearchTerm(event.target.value)}} id="search" />
-      {<h3>{searchTerm} ({data.length})</h3>}
+      <input type="text" onChange={event => {setSearchTerm(event.target.value); setType("search")}} id="search" />
+      {data && `<h3>There are ${data.size} drinks "${searchTerm}"</h3>`}
       <div className="drinks-holder">
         {data && (
           data.drinks.map((drink, i) =>
             <div className="drink-holder" key={i}>
               <h2 className="drink__title">{drink.strDrink}</h2>
               <img src={drink.strDrinkThumb} className="drink__img" />
-              {getIngredientsList(drink)}
               <p className="drink__view-more" onClick={() => setViewSingleDrink(drink.idDrink)}>More about this drink</p>
             </div>
           )
-        )}
+        )
+        }
       </div>
-    </>
-  )
+      </>
+    )
+  }
+
+  const searchDrinks = () => {
+    return (
+      <>
+      <h1>{`There are ${data.size} drinks "${searchTerm}"</h3>`}</h1>
+      <button onClick={() => {fetchRandomDrinks(); setType("random")}}>Get a random cocktail</button>
+      <label htmlFor="search">Search by ingredient</label>
+      <input type="text" onChange={event => {setSearchTerm(event.target.value); setType("search")}} id="search" />
+      <div className="drinks-holder">
+        {data && (
+          data.drinks.map((drink, i) =>
+            <div className="drink-holder" key={i}>
+              <h2 className="drink__title">{drink.strDrink}</h2>
+              <img src={drink.strDrinkThumb} className="drink__img" />
+              <p className="drink__view-more" onClick={() => setViewSingleDrink(drink.idDrink)}>More about this drink</p>
+            </div>
+          )
+        )
+        }
+      </div>
+      </>
+    )
+  }
+
+  switch(type){
+    case "random":
+      return getRandomDrinks()
+    case "search":
+      return searchDrinks()
+    break
+  }
+
 }
 
 export default App;
-
-const getIngredientsList = drink => {
-  // return drink.map((item, i) => <li>{}</li>)
-}
